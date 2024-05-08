b5d232d1e6d3d3944d5660d77c1a413295815b2e
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-05-08 08:58:23 -0600

    test user model

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,38 +1,7 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
-
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def create_user(self, nombre_usuario, email, password=None, **extra_fields):
-        """
-        Create and save a User with the given username, email, and password.
-        """
-        if not nombre_usuario:
-            raise ValueError(_('The Username must be set'))
-        if not email:
-            raise ValueError(_('The Email must be set'))
-        email = self.normalize_email(email)
-        user = self.model(nombre_usuario=nombre_usuario, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, nombre_usuario, email, password=None, **extra_fields):
-        """
-        Create and save a SuperUser with the given username, email, and password.
-        """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if not extra_fields.get('is_staff'):
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if not extra_fields.get('is_superuser'):
-            raise ValueError(_('Superuser must have is_superuser=True.'))
-
-        return self.create_user(nombre_usuario, email, password, **extra_fields)
+from django.utils import timezone
 
 class User(AbstractBaseUser, PermissionsMixin):
     nombre_usuario = models.CharField(_('username'), max_length=150, unique=True)
@@ -42,10 +11,35 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(_('active'), default=True)
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
-    objects = UserManager()
-
     USERNAME_FIELD = 'nombre_usuario'
     REQUIRED_FIELDS = ['email']
 
+    objects = UserManager()
+
     def __str__(self):
         return self.nombre_usuario
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, nombre_usuario, email, password=None, **extra_fields):
+        if not nombre_usuario:
+            raise ValueError(_('The Username must be set'))
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(nombre_usuario=nombre_usuario, email=email, **extra_fields)  # This should refer to the User model
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, nombre_usuario, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(nombre_usuario, email, password, **extra_fields)
