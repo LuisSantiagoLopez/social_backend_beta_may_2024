147867db299b040e5c83ca9a0dfab9a40d05e707
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-05-08 09:00:49 -0600

    test user model

---
+++ usuarios/manager.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, nombre_usuario, email, password=None, **extra_fields):
+        if not nombre_usuario:
+            raise ValueError(_('The Username must be set'))
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(nombre_usuario=nombre_usuario, email=email, **extra_fields)  # This should refer to the User model
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, nombre_usuario, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(nombre_usuario, email, password, **extra_fields)
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .manager import UserManager
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
@@ -19,27 +20,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.nombre_usuario
 
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def create_user(self, nombre_usuario, email, password=None, **extra_fields):
-        if not nombre_usuario:
-            raise ValueError(_('The Username must be set'))
-        if not email:
-            raise ValueError(_('The Email must be set'))
-        email = self.normalize_email(email)
-        user = self.model(nombre_usuario=nombre_usuario, email=email, **extra_fields)  # This should refer to the User model
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, nombre_usuario, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
-
-        return self.create_user(nombre_usuario, email, password, **extra_fields)
