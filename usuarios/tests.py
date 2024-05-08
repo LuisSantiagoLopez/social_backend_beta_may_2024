a58b2c72df872ef3e966349a0083595b82326e12
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-05-08 08:39:34 -0600

    test user model

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -6,7 +6,7 @@ class UsersManagersTests(TestCase):
 
     def test_create_user(self):
         User = get_user_model()
-        user = User.objects.create_user(email="normal@user.com", password="foo")
+        user = User.objects.create_user(nombre_usuario="normal",email="normal@user.com", password="foo")
         self.assertEqual(user.email, "normal@user.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
