5b89be4c3343c6647624e0e08c5967172ddcbcfb
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-05-08 09:01:36 -0600

    test user model

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from .models import User, UserManager
+from .models import User
+from .manager import UserManager
 
 class UserTestCase(TestCase):
     def setUp(self):
