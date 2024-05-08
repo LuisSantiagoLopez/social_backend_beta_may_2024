e807bb0149268482ab8c74ebd3818cfb9228a9ca
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-05-08 13:05:45 -0600

    registrar usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,6 +3,6 @@ from .views import usuarios, registrar_usuario
 
 
 urlpatterns = [
- path("/usuarios", usuarios, name="usuarios"),
- path("/registrar_usuario", registrar_usuario, name="registrar_usuario")
+ path("usuarios", usuarios, name="usuarios"),
+ path("registrar_usuario", registrar_usuario, name="registrar_usuario")
 ]
